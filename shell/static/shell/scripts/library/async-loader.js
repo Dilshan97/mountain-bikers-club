1e8bb8e4725dfce5fb94425d0d04ee3e08a4713d
Cédric Eberhardt <design+code@cedeber.fr>
2018-10-20 15:24:16 +0200

    update async-loader

--- shell/static/shell/scripts/library/async-loader.js
+++ shell/static/shell/scripts/library/async-loader.js
@@ -1,5 +1,7 @@
 const urlList = new Set();
+/** @type Promise<*> */
 let loadJSPromise = Promise.resolve();
+/** @type Promise<*> */
 let loadCSSPromise = Promise.resolve();
 
 /**
@@ -9,26 +11,22 @@ let loadCSSPromise = Promise.resolve();
  * @param {string} [integrity='']
  * @returns {Promise<*>}
  */
-export function loadJS(src, crossOrigin = false, integrity = "") {
+export function loadJS(src, crossOrigin = false, integrity = '') {
     if (urlList.has(src)) {
         return loadJSPromise;
     }
 
     loadJSPromise = new Promise((resolve, reject) => {
-        const script = document.createElement("script");
+        const script = document.createElement('script');
 
-        script.onload = event => resolve(event);
-        script.onerror = event => reject(event);
+        script.addEventListener('load', event => resolve(event));
+        script.addEventListener('error', event => reject(event));
         script.async = true;
-
-        if (typeof crossOrigin === "string") {
-            script.crossOrigin = crossOrigin;
-        } else if (crossOrigin) {
-            script.crossOrigin = "anonymous";
+        if (typeof crossOrigin === 'string') {
+            script.setAttribute('crossorigin', crossOrigin);
         }
-
-        script.integrity = integrity;
-        script.src = src;
+        script.setAttribute('integrity', integrity);
+        script.setAttribute('src', src);
 
         document.head.appendChild(script);
         urlList.add(src);
@@ -40,32 +38,35 @@ export function loadJS(src, crossOrigin = false, integrity = "") {
 /**
  * Load CSS asynchronously
  * @param {string} src
- * @param {string|boolean} [crossOrigin=false]
+ * @param {boolean|string} [crossOrigin=false]
  * @param {string} [integrity='']
  * @param {string} [media="screen"]
  * @returns {Promise<*>}
  */
-export function loadCSS(src, crossOrigin = false, integrity = "", media = "screen") {
+export function loadCSS(
+    src,
+    crossOrigin = false,
+    integrity = '',
+    media = 'screen',
+) {
     if (urlList.has(src)) {
         return loadCSSPromise;
     }
 
     loadCSSPromise = new Promise((resolve, reject) => {
-        const link = document.createElement("link");
+        const link = document.createElement('link');
 
-        link.addEventListener("load", event => resolve(event));
-        link.addEventListener("error", event => reject(event));
-        link.setAttribute("rel", "stylesheet");
-        link.setAttribute("media", media);
+        link.addEventListener('load', event => resolve(event));
+        link.addEventListener('error', event => reject(event));
+        link.setAttribute('rel', 'stylesheet');
+        link.setAttribute('media', media);
 
-        if (typeof crossOrigin === "string") {
-            link.crossOrigin = crossOrigin;
-        } else if (crossOrigin) {
-            link.crossOrigin = "anonymous";
+        if (typeof crossOrigin === 'string') {
+            link.setAttribute('crossorigin', crossOrigin);
         }
 
-        link.integrity = integrity;
-        link.setAttribute("href", src);
+        link.setAttribute('integrity', integrity);
+        link.setAttribute('href', src);
 
         document.head.appendChild(link);
         urlList.add(src);
