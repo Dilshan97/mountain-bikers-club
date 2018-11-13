8b125623ade3b70c821dd1a4364dc9c504065b67
Cédric Eberhardt <design+code@cedeber.fr>
2018-11-13 17:27:25 +0100

    code style

--- trail/signals.py
+++ trail/signals.py
@@ -4,10 +4,9 @@ from django.dispatch import receiver
 from .models import Trail
 
 
-# https://stackoverflow.com/questions/16041232/django-delete-filefield
 @receiver(pre_delete, sender=Trail)
 def trail_delete_handler(sender, **kwargs):
-    instance = kwargs['instance']
+    instance = kwargs["instance"]
     instance.file.delete()
     instance.thumbnail.delete()
     instance.hero.delete()
